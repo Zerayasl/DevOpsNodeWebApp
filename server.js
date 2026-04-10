bd3d339c645ca07c2169c4cc491a768fc19a8d58
Zerayasl <Zerayasl@students.zhaw.ch>
2026-04-10 19:37:21 +0200

    DevOps 06 Quality

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2026 DevOps Course!');
+  // angepasst von zerayasl
+  res.send('Hello FS2026 DevOps Course! - Dockerized version');
 });
 
 app.listen(PORT, HOST);
