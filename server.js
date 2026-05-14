32f8c3f3abe00f573a7adf73856d32ee1ad19d12
Zerayasl <Zerayasl@students.zhaw.ch>
2026-05-14 18:48:29 +0200

    dsf

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
   // angepasst von zerayasl
-  res.send('Hello FS2026 DevOps Course! - dini muetr');
+  res.send('Hello FS2026 DevOps Course! - vallah sheisse');
 });
 
 app.listen(PORT, HOST);
